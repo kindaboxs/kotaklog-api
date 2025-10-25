7c2ae72183a410944a67c4627285ba9531176ae8
MrBoxs <rizkywanda200155@gmail.com>
2025-10-25 20:58:21 +0700

    feat: add Better Auth dependencies
    Added Better Auth packages including @better-auth/core, @better-auth/telemetry, @better-auth/utils, and related utilities to enable authentication features in the project.

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,13 @@
+/**
+ * Custom modules
+ */
+import { auth } from '@/lib/auth';
+import factory from '@/lib/factory';
+
+const authRoute = factory.createApp().basePath('/auth');
+
+authRoute.on(['POST', 'GET'], '/*', c => {
+  return auth.handler(c.req.raw);
+});
+
+export default authRoute;
